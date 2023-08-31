b410f9778604e6d63cf2c6251bf579b1a9f71ced
Gary Kim <gary@garykim.dev>
2023-08-31 12:50:03 -0400

    Add Mudbug playbooks

---
+++ roles/common/res/ulab.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ -d "/afs/ee.cooper.edu/dist" ]; then
+    source /afs/ee.cooper.edu/dist/rc
+fi
+/afs/ee.cooper.edu/dist/misc/greet.sh
+
---
+++ roles/fs/auristorfs/res/krenew.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+aklog ee.cooper.edu
+/usr/bin/krenew -a -b -K 60 -L -t
+cd "$HOME"
+
